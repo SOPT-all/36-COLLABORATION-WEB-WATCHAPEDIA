2a7372322e644042d8a1cc663e36c89e8028b2f1
yeonilil <gwagseoyeon@gmail.com>
2025-05-12 08:01:43 +0900

    refactor: 폰트 스타일 네이밍 수정 적용

--- src/app/styles.css.ts
+++ src/app/styles.css.ts
@@ -54,146 +54,150 @@ const createFontStyle = ({
   })
 
 export const font = {
-  // Bold
-  b_36: createFontStyle({
+  // headline
+  headline_b_36: createFontStyle({
     fontSize: '36px',
     fontWeight: '700',
     lineHeight: '44px',
   }),
+  headline_b_24: createFontStyle({
+    fontSize: '24px',
+    fontWeight: '700',
+    lineHeight: '36px',
+  }),
+  headline_sb_16: createFontStyle({
+    fontSize: '16px',
+    fontWeight: '600',
+    lineHeight: '24px',
+  }),
+
+  // title
+  title_sb_20: createFontStyle({
+    fontSize: '20px',
+    fontWeight: '600',
+    lineHeight: '24px',
+  }),
+  title_sb_14: createFontStyle({
+    fontSize: '14px',
+    fontWeight: '600',
+    lineHeight: '16px',
+  }),
+  title_m_14: createFontStyle({
+    fontSize: '14px',
+    fontWeight: '500',
+    lineHeight: '20px',
+  }),
+  title_r_28: createFontStyle({
+    fontSize: '28px',
+    fontWeight: '400',
+    lineHeight: '42px',
+  }),
+
+  // subtitle
+  subtitle_b_17: createFontStyle({
+    fontSize: '17px',
+    fontWeight: '700',
+    lineHeight: '20px',
+  }),
+  subtitle_sb_17: createFontStyle({
+    fontSize: '17px',
+    fontWeight: '600',
+    lineHeight: '26px',
+  }),
+  subtitle_m_28: createFontStyle({
+    fontSize: '28px',
+    fontWeight: '500',
+    lineHeight: '42px',
+  }),
+  subtitle_m_18: createFontStyle({
+    fontSize: '18px',
+    fontWeight: '500',
+    lineHeight: '24px',
+  }),
+  subtitle_m_17: createFontStyle({
+    fontSize: '17px',
+    fontWeight: '500',
+    lineHeight: '26px',
+  }),
+  subtitle_m_13: createFontStyle({
+    fontSize: '13px',
+    fontWeight: '500',
+    lineHeight: '16px',
+  }),
+  subtitle_r_14: createFontStyle({
+    fontSize: '14px',
+    fontWeight: '400',
+    lineHeight: '20px',
+  }),
+
+  // body
+  body_sb_13: createFontStyle({
+    fontSize: '13px',
+    fontWeight: '600',
+    lineHeight: '16px',
+  }),
+  body_m_16: createFontStyle({
+    fontSize: '16px',
+    fontWeight: '500',
+    lineHeight: '20px',
+  }),
+  body_m_15: createFontStyle({
+    fontSize: '15px',
+    fontWeight: '500',
+    lineHeight: '20px',
+  }),
+  body_r_16: createFontStyle({
+    fontSize: '16px',
+    fontWeight: '400',
+    lineHeight: '22px',
+  }),
+  body_r_15: createFontStyle({
+    fontSize: '15px',
+    fontWeight: '400',
+    lineHeight: '16px',
+  }),
+  body_r_14: createFontStyle({
+    fontSize: '14px',
+    fontWeight: '400',
+    lineHeight: '20px',
+  }),
+  body_r_11: createFontStyle({
+    fontSize: '11px',
+    fontWeight: '400',
+    lineHeight: '12px',
+  }),
+
+  // label
+  label_m_12: createFontStyle({
+    fontSize: '12px',
+    fontWeight: '500',
+    lineHeight: '14px',
+  }),
+  label_m_10: createFontStyle({
+    fontSize: '10px',
+    fontWeight: '500',
+    lineHeight: '20px',
+  }),
+  label_r_12: createFontStyle({
+    fontSize: '12px',
+    fontWeight: '400',
+    lineHeight: '14px',
+  }),
+  label_r_10: createFontStyle({
+    fontSize: '10px',
+    fontWeight: '400',
+    lineHeight: '14px',
+  }),
+
+  // 기타
   b_25: createFontStyle({
     fontSize: '25px',
     fontWeight: '700',
     lineHeight: '32px',
   }),
-  b_24_2: createFontStyle({
-    fontSize: '24px',
-    fontWeight: '700',
-    lineHeight: '36px',
-  }),
-  b_17: createFontStyle({
-    fontSize: '17px',
-    fontWeight: '700',
-    lineHeight: '20px',
-  }),
-
-  // Semibold
-  sb_20_2: createFontStyle({
-    fontSize: '20px',
-    fontWeight: '600',
-    lineHeight: '24px',
-  }),
-  sb_17_2: createFontStyle({
-    fontSize: '17px',
-    fontWeight: '600',
-    lineHeight: '26px',
-  }),
-  sb_16: createFontStyle({
-    fontSize: '16px',
-    fontWeight: '600',
-    lineHeight: '24px',
-  }),
-  sb_14: createFontStyle({
-    fontSize: '14px',
-    fontWeight: '600',
-    lineHeight: '16px',
-  }),
-  sb_13: createFontStyle({
-    fontSize: '13px',
-    fontWeight: '600',
-    lineHeight: '16px',
-  }),
-
-  // Medium
   m_36: createFontStyle({
     fontSize: '36px',
     fontWeight: '500',
     lineHeight: '36px',
-  }),
-  m_28_2: createFontStyle({
-    fontSize: '28px',
-    fontWeight: '500',
-    lineHeight: '42px',
-  }),
-  m_18: createFontStyle({
-    fontSize: '18px',
-    fontWeight: '500',
-    lineHeight: '24px',
-  }),
-  m_17_2: createFontStyle({
-    fontSize: '17px',
-    fontWeight: '500',
-    lineHeight: '26px',
-  }),
-  m_16: createFontStyle({
-    fontSize: '16px',
-    fontWeight: '500',
-    lineHeight: '20px',
-  }),
-  m_15: createFontStyle({
-    fontSize: '15px',
-    fontWeight: '500',
-    lineHeight: '20px',
-  }),
-  m_14: createFontStyle({
-    fontSize: '14px',
-    fontWeight: '500',
-    lineHeight: '20px',
-  }),
-  m_13: createFontStyle({
-    fontSize: '13px',
-    fontWeight: '500',
-    lineHeight: '16px',
-  }),
-  m_12: createFontStyle({
-    fontSize: '12px',
-    fontWeight: '500',
-    lineHeight: '14px',
-  }),
-  m_10: createFontStyle({
-    fontSize: '10px',
-    fontWeight: '500',
-    lineHeight: '20px',
-  }),
-
-  // Regular
-  r_28_2: createFontStyle({
-    fontSize: '28px',
-    fontWeight: '400',
-    lineHeight: '42px',
-  }),
-  r_16: createFontStyle({
-    fontSize: '16px',
-    fontWeight: '400',
-    lineHeight: '22px',
-  }),
-  r_15: createFontStyle({
-    fontSize: '15px',
-    fontWeight: '400',
-    lineHeight: '16px',
-  }),
-  r_14: createFontStyle({
-    fontSize: '14px',
-    fontWeight: '400',
-    lineHeight: '20px',
-  }),
-  r_14_2: createFontStyle({
-    fontSize: '14px',
-    fontWeight: '400',
-    lineHeight: '20px',
-  }),
-  r_12: createFontStyle({
-    fontSize: '12px',
-    fontWeight: '400',
-    lineHeight: '14px',
-  }),
-  r_11: createFontStyle({
-    fontSize: '11px',
-    fontWeight: '400',
-    lineHeight: '12px',
-  }),
-  r_10: createFontStyle({
-    fontSize: '10px',
-    fontWeight: '400',
-    lineHeight: '14px',
   }),
 }
