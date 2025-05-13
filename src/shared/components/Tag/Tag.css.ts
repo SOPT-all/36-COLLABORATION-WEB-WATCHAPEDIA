e889b7d8114dc84200e4af44d5b1b710de51bce7
Jeong-Ag <kjungu1105@gmail.com>
2025-05-13 20:30:40 +0900

    feat: RankTag 추가

---
+++ src/shared/components/Tag/Tag.css.ts
@@ -0,0 +1,13 @@
+import { style } from '@vanilla-extract/css'
+
+export const tag = style({
+  display: 'flex',
+  padding: '3px 8px',
+  borderRadius: '3px',
+  textAlign: 'center',
+  alignItems: 'center',
+  backgroundColor: 'black',
+  gap: '5px',
+  color: 'white',
+  opacity: '80%',
+})
---
+++ src/shared/components/Tag/Tag.tsx
@@ -0,0 +1,37 @@
+import { RankDown, RankSame, RankUp } from '@/assets/svg'
+import clsx from 'clsx'
+import { font } from '@/app/styles.css'
+import { tag } from './tag.css'
+
+export const RANK_STATE = {
+  UP: 'up',
+  DOWN: 'down',
+  SAME: 'same',
+} as const
+
+export type MovieState = (typeof RANK_STATE)[keyof typeof RANK_STATE]
+
+interface RankTagProps {
+  rank: number | string
+  state: MovieState
+}
+
+export const RankTag = ({ rank, state }: RankTagProps) => {
+  const stateIcon = () => {
+    switch (state) {
+      case RANK_STATE.UP:
+        return <RankUp />
+      case RANK_STATE.DOWN:
+        return <RankDown />
+      case RANK_STATE.SAME:
+        return <RankSame />
+    }
+  }
+
+  return (
+    <div className={clsx(tag, font.body_sb_13)}>
+      {rank}
+      {stateIcon()}
+    </div>
+  )
+}
