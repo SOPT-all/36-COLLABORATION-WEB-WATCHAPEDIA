45d9adcbb57b0a531a6c10d127898d9dedadbdc6
Jeong-Ag <kjungu1105@gmail.com>
2025-05-14 00:53:35 +0900

    refactor: state 삭제 후 Tag 하나로 통합

--- src/shared/components/Tag/Tag.css.ts
+++ src/shared/components/Tag/Tag.css.ts
@@ -1,13 +1,36 @@
-import { style } from '@vanilla-extract/css'
+import { recipe } from '@vanilla-extract/recipes'
 
-export const tag = style({
-  display: 'flex',
-  padding: '3px 8px',
-  borderRadius: '3px',
-  textAlign: 'center',
-  alignItems: 'center',
-  backgroundColor: 'black',
-  gap: '5px',
-  color: 'white',
-  opacity: '80%',
+export const tag = recipe({
+  base: {
+    display: 'flex',
+    textAlign: 'center',
+    alignItems: 'center',
+    width: 'fit-content',
+  },
+
+  variants: {
+    color: {
+      blue: {
+        backgroundColor: '#EEF7FF',
+        color: '#003A4E',
+      },
+      orange: {
+        color: '#9B3800',
+        backgroundColor: '#FFF1E9',
+      },
+      gray: {
+        backgroundColor: '#F8F8F8',
+        color: '#5F5F5F',
+      },
+      black: {
+        backgroundColor: 'black',
+        color: 'white',
+        opacity: '80%',
+      },
+    },
+    size: {
+      sm: { padding: '3px 8px', borderRadius: '3px' },
+      lg: { padding: '6px 10px', borderRadius: '4px' },
+    },
+  },
 })
--- src/shared/components/Tag/Tag.tsx
+++ src/shared/components/Tag/Tag.tsx
@@ -1,45 +1,15 @@
-import { RankDown, RankSame, RankUp } from '@/assets/svg'
 import clsx from 'clsx'
 import { font } from '@/app/styles.css'
-import { tag } from './tag.css'
+import { tag } from './Tag.css'
 
-export const RANK_STATE = {
-  UP: 'up',
-  DOWN: 'down',
-  SAME: 'same',
-} as const
-
-export type MovieState = (typeof RANK_STATE)[keyof typeof RANK_STATE]
-
-interface RankTagProps {
-  rank: number | string
-  state: MovieState
-}
-
-interface TextTagProps {
+interface TagProps {
   text: string
+  color?: 'blue' | 'orange' | 'gray' | 'black'
+  size?: 'sm' | 'lg'
 }
 
-export const RankTag = ({ rank, state }: RankTagProps) => {
-  const stateIcon = () => {
-    switch (state) {
-      case RANK_STATE.UP:
-        return <RankUp />
-      case RANK_STATE.DOWN:
-        return <RankDown />
-      case RANK_STATE.SAME:
-        return <RankSame />
-    }
-  }
+export const Tag = ({ text, color = 'black', size='lg' }: TagProps) => {
+  const typo = color === 'black' ? font.body_sb_13 : font.label_r_10
 
-  return (
-    <div className={clsx(tag, font.body_sb_13)}>
-      {rank}
-      {stateIcon()}
-    </div>
-  )
-}
-
-export const TextTag = ({ text }: TextTagProps) => {
-  return <div className={clsx(tag, font.body_sb_13)}>{text}</div>
+  return <div className={clsx(tag({ color, size }), typo)}>{text}</div>
 }
