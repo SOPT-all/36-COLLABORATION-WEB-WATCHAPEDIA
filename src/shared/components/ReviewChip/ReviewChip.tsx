23e45ea86171be7fc1eed7c11d2e72f2277b0d10
Jeong-Ag <kjungu1105@gmail.com>
2025-05-13 15:58:55 +0900

    feat: `ReviewChip` 생성

---
+++ src/shared/components/ReviewChip/ReviewChip.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx'
+import { reviewChip } from './styles.css'
+import { font } from '@/app/styles.css'
+import { ButtonHTMLAttributes } from 'react'
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string
+  isSelected: boolean
+}
+
+const ReviewChip = ({ text, isSelected = false, ...props }: Props) => {
+  const typography = isSelected ? font.label_m_12 : font.label_r_12
+
+  return (
+    <button className={clsx(reviewChip({ isSelected }), typography)} {...props}>
+      {text}
+    </button>
+  )
+}
+
+export default ReviewChip
---
+++ src/shared/components/ReviewChip/styles.css.ts
@@ -0,0 +1,40 @@
+import { color } from '@/app/styles.css'
+import { recipe } from '@vanilla-extract/recipes'
+
+export const reviewChip = recipe({
+  base: {
+    display: 'flex',
+    padding: '10px 12px',
+    borderRadius: '4px',
+    border: 'none',
+    textAlign: 'center',
+    height: '30px',
+    alignItems: 'center',
+    transition:
+      'background-color 0.3s ease, opacity 0.3s ease, cursor 0.3s ease',
+    cursor: 'pointer',
+    color: color.gray.gray6,
+    backgroundColor: color.background.subtitle,
+
+    selectors: {
+      '&:disabled': {
+        opacity: '20%',
+        cursor: 'not-allowed',
+      },
+    },
+  },
+
+  variants: {
+    isSelected: {
+      false: {
+        color: color.gray.gray6,
+      },
+      true: {
+        backgroundColor: color.brand.primaryWhite,
+        color: color.brand.primary,
+        border: '1px solid',
+        borderColor: color.brand.primary,
+      },
+    },
+  },
+})
