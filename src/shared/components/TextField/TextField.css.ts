f14543899eaa5b1985f0e41087f2bae819bf6881
Jeong-Ag <kjungu1105@gmail.com>
2025-05-14 15:53:40 +0900

    refactor: 폰트 적용 방식 수정

--- src/shared/components/TextField/TextField.css.ts
+++ src/shared/components/TextField/TextField.css.ts
@@ -1,25 +1,28 @@
-import { color } from '@/app/styles.css'
+import { color, font } from '@/app/styles.css'
 import { style } from '@vanilla-extract/css'
 
-export const textField = style({
-  color: color.gray.gray7,
-  width: '100%',
-  height: '100%',
-  background: 'none',
-  border: 'none',
-  outline: 'none',
-  resize: 'none',
+export const textField = style([
+  font.subtitle_r_14,
+  {
+    color: color.gray.gray7,
+    width: '100%',
+    height: '100%',
+    background: 'none',
+    border: 'none',
+    outline: 'none',
+    resize: 'none',
 
-  selectors: {
-    '&::placeholder': {
-      color: color.gray.gray4,
-    },
-    '&::-webkit-scrollbar': {
-      display: 'none',
-    },
-    '&': {
-      scrollbarWidth: 'none',
-      msOverflowStyle: 'none',
+    selectors: {
+      '&::placeholder': {
+        color: color.gray.gray4,
+      },
+      '&::-webkit-scrollbar': {
+        display: 'none',
+      },
+      '&': {
+        scrollbarWidth: 'none',
+        msOverflowStyle: 'none',
+      },
     },
   },
-})
+])
--- src/shared/components/TextField/TextField.tsx
+++ src/shared/components/TextField/TextField.tsx
@@ -1,12 +1,10 @@
-import clsx from 'clsx'
 import { TextareaHTMLAttributes } from 'react'
 import { textField } from './TextField.css'
-import { font } from '@/app/styles.css'
 
 interface Props extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 const TextField = ({ ...props }: Props) => {
-  return <textarea className={clsx(textField, font.subtitle_r_14)} {...props} />
+  return <textarea className={textField} {...props} />
 }
 
 export default TextField
