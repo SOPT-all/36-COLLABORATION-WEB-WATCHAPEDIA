a8fa8a972fbd54a5dd45290bf0d9cd7f2eeda01c
Jeong-Ag <kjungu1105@gmail.com>
2025-05-16 22:30:01 +0900

    feat: 뒤로가기 기능 추가

--- src/app/content/[id]/components/info/InfoHeader/InfoHeader.css.ts
+++ src/app/content/[id]/components/info/InfoHeader/InfoHeader.css.ts
@@ -1,9 +1,13 @@
 import { style } from '@vanilla-extract/css'
 
-export const InfoHeaderContainer = style({
+export const infoHeaderContainer = style({
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
   height: 48,
   boxSizing: 'border-box',
+})
+
+export const infoHeaderBackButton = style({
+  all: 'unset',
 })
--- src/app/content/[id]/components/info/InfoHeader/InfoHeader.tsx
+++ src/app/content/[id]/components/info/InfoHeader/InfoHeader.tsx
@@ -1,11 +1,22 @@
+'use client'
+
 import SvgBack from '@/assets/svg/Back'
 import SvgShare from '@/assets/svg/Share'
 import * as styles from './InfoHeader.css'
+import { useRouter } from 'next/navigation'
 
 const InfoHeader = () => {
+  const router = useRouter()
+
   return (
-    <div className={styles.InfoHeaderContainer}>
-      <SvgBack width={22} height={22} />
+    <div className={styles.infoHeaderContainer}>
+      <button
+        type="button"
+        className={styles.infoHeaderBackButton}
+        onClick={() => router.back()}
+      >
+        <SvgBack width={22} height={22} />
+      </button>
       <SvgShare width={24} height={24} />
     </div>
   )
