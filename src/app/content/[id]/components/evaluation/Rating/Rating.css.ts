bd47ef47de890413e9197c414efa88f9f4f08f8c
Jeong-Ag <kjungu1105@gmail.com>
2025-05-20 00:24:30 +0900

    feat: Rating 컴포넌트 및 스타일 추가

---
+++ src/app/content/[id]/components/evaluation/Rating/Rating.css.ts
@@ -0,0 +1,60 @@
+import { color } from '@/app/styles.css'
+import { style } from '@vanilla-extract/css'
+import { recipe } from '@vanilla-extract/recipes'
+
+export const ratingContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 20,
+  padding: '20px 0 0 0',
+  alignSelf: 'stretch',
+  justifyContent: 'center',
+  alignItems: 'center',
+})
+
+export const ratingStarContainer = style({
+  display: 'flex',
+  width: 'fit-content',
+  padding: '0 10px',
+})
+
+export const ratingStar = recipe({
+  base: {
+    display: 'inline-block',
+  },
+  variants: {
+    isFull: {
+      true: { color: color.brand.primary },
+      false: { color: color.gray.gray2 },
+    },
+    isHalf: {
+      true: { color: color.brand.primary },
+      false: { color: color.gray.gray2 },
+    },
+  },
+})
+
+export const overlayStarHalf = style({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '50%',
+  height: '100%',
+  overflow: 'hidden',
+  color: color.brand.primary,
+  transition: 'color 0.3s ease-in-out',
+})
+
+export const baseStar = recipe({
+  base: {
+    position: 'relative',
+    display: 'inline-block',
+    transition: 'color 0.3s ease-in-out',
+  },
+  variants: {
+    isFull: {
+      true: { color: color.brand.primary },
+      false: { color: color.gray.gray2 },
+    },
+  },
+})
