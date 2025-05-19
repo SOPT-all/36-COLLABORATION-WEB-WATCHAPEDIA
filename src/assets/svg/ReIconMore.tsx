5c70998fe96da35263bf374a798d3323797c4158
Jeong-Ag <kjungu1105@gmail.com>
2025-05-19 22:37:06 +0900

    feat: react button 컴포넌트 및 아이콘 추가

---
+++ src/assets/svg/ReIconMore.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import type { SVGProps } from 'react'
+const SvgReIconMore = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={28}
+    height={28}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M5.5 12a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5m8.462 0a2.5 2.5 0 1 1-.001 5 2.5 2.5 0 0 1 0-5m8.46 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5"
+    />
+  </svg>
+)
+export default SvgReIconMore
