5fa2b565d396aaf9f05e99735e4d8f1734a665dc
jogpfls <joerin4177@gmail.com>
2025-05-19 21:46:12 +0900

    feat: 그래프 TextContent 구현

---
+++ src/app/content/[id]/components/StarGraph/GraphTextContent/GraphTextContent.css.ts
@@ -0,0 +1,46 @@
+import { color, font } from "@/app/styles.css"
+import { style } from "@vanilla-extract/css"
+
+export const starGraphTextWrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '8px',
+  marginBottom: '6px',
+})
+
+export const bottomText = style({
+  display: 'flex',
+})
+
+export const averageWrapper = style({
+  display: 'flex',
+})
+
+export const averageText = style([
+  font.subtitle_m_18,
+  {
+    color: color.gray.gray6,
+  }
+])
+
+export const graphStar = style({
+  display: 'flex',
+  alignItems: 'flex-start',
+})
+
+export const explainText = style([
+  font.label_r_12,
+  {
+    color: color.gray.gray6,
+  }
+])
+
+export const explainCountText = style([
+  font.label_r_12,
+  {
+    color: color.gray.gray6,
+    display: 'flex',
+    alignItems: 'flex-end',
+    paddingBottom: '3px'
+  }
+])
---
+++ src/app/content/[id]/components/StarGraph/GraphTextContent/GraphTextContent.tsx
@@ -0,0 +1,26 @@
+import * as styles from './GraphTextContent.css'
+import { IcGraphStar } from '@/assets/svg';
+
+const GraphTextContent = () => {
+
+  const STAR_GRAPH_DATA = {
+    averageRating: 3.7,
+    reviewCount: '8.8만명',
+  };
+
+  return (
+    <div className={styles.starGraphTextWrapper}>
+        <p className={styles.explainText}>별점 그래프</p>
+        <div className={styles.bottomText}>
+          <div className={styles.averageWrapper}>
+            <p className={styles.averageText}>평균&nbsp;</p>
+            <div className={styles.graphStar}><IcGraphStar/></div>
+            <p className={styles.averageText}>{STAR_GRAPH_DATA.averageRating}</p>
+          </div>
+          <p className={styles.explainCountText}>{STAR_GRAPH_DATA.reviewCount}</p>
+        </div>
+      </div>
+  );
+};
+
+export default GraphTextContent;
