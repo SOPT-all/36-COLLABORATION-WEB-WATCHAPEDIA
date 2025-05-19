1c56200de289dc5d4aa21cf0e38802b77287f627
Jeong-Ag <kjungu1105@gmail.com>
2025-05-19 21:44:50 +0900

    design: 정보 섹션 레이아웃 조정

--- src/app/content/[id]/layout.css.ts
+++ src/app/content/[id]/layout.css.ts
@@ -3,6 +3,4 @@ import { style } from '@vanilla-extract/css'
 export const layoutWrapper = style({
   display: 'flex',
   flexDirection: 'column',
-  padding: '10px 15px',
-  gap: 49,
 })
--- src/app/content/[id]/sections/info/InfoSection.tsx
+++ src/app/content/[id]/sections/info/InfoSection.tsx
@@ -1,6 +1,6 @@
 import { detail } from '@/mocks'
 import Image from 'next/image'
-import InfoContent from '../../components/info/InfoContnet/InfoContent'
+import InfoContent from '../../components/info/InfoContent/InfoContent'
 import InfoHeader from '../../components/info/InfoHeader/InfoHeader'
 import * as styles from './InfoSection.css'
 
@@ -17,7 +17,7 @@ const InfoSection = () => {
   } = detail
 
   return (
-    <section>
+    <section className={styles.infoContainer}>
       <Image
         src={posterImage}
         alt={`${title} 사진`}
