cf2050df557316fa8c2c146ef020fa1db3206f9c
jogpfls <joerin4177@gmail.com>
2025-05-20 04:37:59 +0900

    feat: magazineField 컴포넌트 구현

---
+++ src/app/content/[id]/components/Magazine/MagazineField/MagazineField.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import * as styles from './MagazineField.css'
+import { IcMagazineLogo } from '@/assets/svg'
+
+interface magazineProps {
+  magazine: {
+    category: string,
+    title: string,
+    content: string,
+    image: string,
+  }
+}
+
+const MagazineField = ({magazine}: magazineProps) => {
+  return (
+    <section className={styles.magazineFieldWrapper}>
+      <div className={styles.categoryWrapper}>
+        <IcMagazineLogo/>
+        <p className={styles.category}>#{magazine.category}</p>
+      </div>
+      <div className={styles.bottomWrapper}>
+        <div className={styles.titleContentWrapper}>
+          <p className={styles.title}>{magazine.title}</p>
+          <p className={styles.content}>{magazine.content}</p>
+        </div>
+        <div className={styles.imageWrapper}>
+          <Image
+          src={magazine.image}
+          alt={`${magazine.category} 기사 사진`}
+          width={105}
+          height={61}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MagazineField;
