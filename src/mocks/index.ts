8d6b608902bea6b75e12caddcb11ac72977cfae2
jogpfls <joerin4177@gmail.com>
2025-05-20 03:35:16 +0900

    feat: comment mocks 데이터 추가

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,5 +1,7 @@
 import detail from './detail.json'
 import trendingComment from './trendingComment.json'
+import comment from './comment.json'
 
 export { detail }
 export { trendingComment }
+export { comment }
