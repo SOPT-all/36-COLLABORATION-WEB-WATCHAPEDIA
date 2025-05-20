38042f88370f5f92b28decef950006a93eb9dfb8
Jeong-Ag <kjungu1105@gmail.com>
2025-05-21 00:33:47 +0900

    feat: 컬렉션 섹션 데이터 추가 및 레이아웃 구현

--- src/app/content/[id]/page.tsx
+++ src/app/content/[id]/page.tsx
@@ -1,9 +1,11 @@
+import CollectionSection from "./sections/collection/CollectionSection"
 import InfoSection from "./sections/info/InfoSection"
 
 const Content = () => {
   return (
     <>
       <InfoSection />
+      <CollectionSection/>
     </>
   )
 }
