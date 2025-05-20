e430514a1a34a2998ed7c8e0659b0914d626088f
Jeong-Ag <kjungu1105@gmail.com>
2025-05-20 23:31:30 +0900

    feat: 동영상 섹션 구현

--- src/app/content/[id]/page.tsx
+++ src/app/content/[id]/page.tsx
@@ -1,11 +1,13 @@
 import GallerySection from './sections/gallery/GallerySection'
 import InfoSection from './sections/info/InfoSection'
+import VideoSection from './sections/video/VideoSection'
 
 const Content = () => {
   return (
     <>
       <InfoSection />
       <GallerySection />
+      <VideoSection />
     </>
   )
 }
---
+++ src/app/content/[id]/sections/video/VideoSection.tsx
@@ -0,0 +1,25 @@
+import SvgIconPlay from '@/assets/svg/IconPlay'
+import HeaderSection from '../../components/HeaderSection/HeaderSection'
+import * as styles from './VideoSection.css'
+import Image from 'next/image'
+
+const VideoSection = () => {
+  return (
+    <HeaderSection title="동영상">
+      <div className={styles.videoContainer}>
+        <Image
+          src="/images/img_vid_1.png"
+          alt="동영상"
+          width={168}
+          height={114}
+        />
+        <div className={styles.videoPlayerOverlay}>
+          <SvgIconPlay width={20} height={20} />
+        </div>
+        <p className={styles.videoPlayerTitle}>메인 예고편</p>
+      </div>
+    </HeaderSection>
+  )
+}
+
+export default VideoSection
