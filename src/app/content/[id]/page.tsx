758c22e478f47320262467ecd4fa7fac968a71a0
Jeong-Ag <kjungu1105@gmail.com>
2025-05-20 22:24:27 +0900

    feat: 영화 설명 섹션 구현

--- src/app/content/[id]/page.tsx
+++ src/app/content/[id]/page.tsx
@@ -1,3 +1,4 @@
+import DescriptionSection from './sections/descriptionSection/DescriptionSection'
 import EvaluationSection from './sections/evaluation/EvaluationSection'
 import InfoSection from './sections/info/InfoSection'
 import * as styles from './styles.css'
@@ -7,6 +8,7 @@ const Content = () => {
     <div className={styles.contentContainer}>
       <InfoSection />
       <EvaluationSection />
+      <DescriptionSection />
     </div>
   )
 }
