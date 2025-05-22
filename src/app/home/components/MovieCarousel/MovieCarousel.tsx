238549dbdc3c51fc6b10927e6744e1722fe585fd
yeonilil <gwagseoyeon@gmail.com>
2025-05-22 22:14:56 +0900

    feat: Top Ranking API 연결

--- src/app/home/components/MovieCarousel/MovieCarousel.tsx
+++ src/app/home/components/MovieCarousel/MovieCarousel.tsx
@@ -4,21 +4,34 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, Autoplay } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/navigation'
-
 import * as styles from '@/app/home/components/MovieCarousel/MovieCarousel.css'
 import MovieCard from '@/shared/widget/MovieCard'
+import { MovieCardPreset } from '@/shared/widget/MovieCard/MovieCard.types'
 
 interface MovieCarouselProps {
   type: 'boxoffice' | 'dDay' | 'series' | 'rank'
   className?: string
   slideContainerClassName?: string
+  data?: MovieCardPreset[]
+}
+
+const chunkArray = <T,>(arr: T[], size: number): T[][] => {
+  const res: T[][] = []
+  for (let i = 0; i < arr.length; i += size) {
+    res.push(arr.slice(i, i + size))
+  }
+  return res
 }
 
 const MovieCarousel = ({
   type,
   className = '',
   slideContainerClassName = '',
+  data = [],
 }: MovieCarouselProps) => {
+  const isDataAvailable = data.length > 0
+  const chunkedData = isDataAvailable ? chunkArray(data, 3) : []
+
   return (
     <div className={`${styles.container} ${className}`}>
       <Swiper
@@ -35,15 +48,25 @@ const MovieCarousel = ({
           disableOnInteraction: false,
         }}
       >
-        {[0, 1].map((_, idx) => (
-          <SwiperSlide key={idx}>
-            <div className={slideContainerClassName}>
-              <MovieCard type={type} />
-              <MovieCard type={type} />
-              <MovieCard type={type} />
-            </div>
-          </SwiperSlide>
-        ))}
+        {isDataAvailable
+          ? chunkedData.map((group, idx) => (
+              <SwiperSlide key={idx} className={styles.swiperSlide}>
+                <div className={slideContainerClassName}>
+                  {group.map((item, i) => (
+                    <MovieCard key={i} type={type} data={item} />
+                  ))}
+                </div>
+              </SwiperSlide>
+            ))
+          : [0, 1].map((_, idx) => (
+              <SwiperSlide key={idx}>
+                <div className={slideContainerClassName}>
+                  <MovieCard type={type} />
+                  <MovieCard type={type} />
+                  <MovieCard type={type} />
+                </div>
+              </SwiperSlide>
+            ))}
       </Swiper>
 
       <div className={`swiper-button-prev ${styles.navButton}`} />
