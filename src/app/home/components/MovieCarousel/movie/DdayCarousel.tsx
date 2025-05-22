c4e2c68b3fd9e3c08b1445462960e9248869c541
yeonilil <gwagseoyeon@gmail.com>
2025-05-22 23:00:55 +0900

    feat: 공개 예정작 API 연결

--- src/app/home/components/MovieCarousel/movie/DdayCarousel.tsx
+++ src/app/home/components/MovieCarousel/movie/DdayCarousel.tsx
@@ -1,11 +1,26 @@
 import * as styles from '@/app/home/components/MovieCarousel/MovieCarousel.css'
 
 import MovieCarousel from '../MovieCarousel'
+import { getdDay } from '@/app/services/getDday'
+import { DDayCard } from '@/shared/widget/MovieCard/MovieCard.types'
 
-const DdayCarousel = () => {
+const DdayCarousel = async () => {
+  const result = await getdDay()
+
+  const data: DDayCard[] = result.map((item, index) => ({
+    id: index,
+    untilRelease: item.untilRelease,
+    isWishedCount: item.isWishedCount,
+    imagePath: item.imagePath,
+    title: item.title,
+    releaseYear: item.releaseYear,
+    type: 'dDay',
+  }))
+
   return (
     <MovieCarousel
       type="dDay"
+      data={data}
       className={styles.container}
       slideContainerClassName={styles.slideContainer}
     />
