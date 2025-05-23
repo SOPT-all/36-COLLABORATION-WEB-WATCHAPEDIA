f470274ea98ff6768be22865e5d40c056bcbb5da
Jeong-Ag <kjungu1105@gmail.com>
2025-05-23 11:59:06 +0900

    fix: 중복 코드 제거

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
 const withVanillaExtract = createVanillaExtractPlugin()
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['an2-img.amz.wtchn.net'], // 외부 이미지 도메인 추가
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
